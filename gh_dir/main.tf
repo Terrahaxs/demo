de327816e68cba3ed0c264937468f1b2d558525c
ghxops <51935312+ghxops@users.noreply.github.com>
2023-05-27 21:03:20 +0000

    style: :lipstick: formatting code with Terraform Format

--- gh_dir/main.tf
+++ gh_dir/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "myfirstbucket" {
   bucket_prefix = "demo-terrahaxs1"
 
   tags = {
-        "Environment" = "staging",
-        "ManagedBy" = "Terraform",
-    }
+    "Environment" = "staging",
+    "ManagedBy"   = "Terraform",
+  }
 }
--- gh_dir0/main.tf
+++ gh_dir0/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "mysecondbucket" {
   bucket_prefix = "demo-terrahaxs2"
 
   tags = {
-        "Environment" = "staging",
-        "ManagedBy" = "Terraform",
-    }
+    "Environment" = "staging",
+    "ManagedBy"   = "Terraform",
+  }
 }
