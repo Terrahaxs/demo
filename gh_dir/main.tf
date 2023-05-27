93edec37096cfef6ba613d2bca5e23931496b58e
Nigel Finley <nfinleymusic@gmail.com>
2023-05-27 17:06:21 -0400

    Removing code

--- gh_dir/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "myfirstbucket" {
-  bucket_prefix = "demo-terrahaxs-5-27-23-1"
-
-  tags = {
-        "Environment" = "staging",
-        "ManagedBy" = "Terraform",
-    }
-}
--- gh_dir0/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "mysecondbucket" {
-  bucket_prefix = "demo-terrahaxs-5-27-23-2"
-
-  tags = {
-        "Environment" = "staging",
-        "ManagedBy" = "Terraform",
-    }
-}
